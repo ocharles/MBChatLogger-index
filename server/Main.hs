c53da42652b4ddb8a7681af16d6c4f15e5dc51bf
Oliver Charles <oliver.g.charles@googlemail.com>
2011-09-27 00:25:22 +0100

    More fixes

--- server/Main.hs
+++ server/Main.hs
@@ -16,6 +16,8 @@ import Data.Monoid (mempty)
 import Data.ByteString.Char8 (unpack)
 import Data.List (genericLength)
 import Data.Maybe
+import Data.Time.Format
+import System.Locale
 
 import MBChatLogger.Index (search)
 import MBChatLogger.Types
@@ -35,8 +37,9 @@ handler = do
       H.h1 "Search"
       H.p "Forget what you were talking about? Don't worry, it happens to me all the time."
       H.p "Supported search fields are..."
-      H.ul $
-        H.li (H.code "user:" >> " Search by nickname")
+      H.ul $ do
+        H.li (H.code "user:" >> " Search by nickname.")
+        H.li (H.code "channel:" >> " Search by channel. You probably want to quote the channel name.")
       H.form ! A.method "GET" $
         H.p $ do
           H.label ! A.for "search-query" $ "Query:"
@@ -52,13 +55,26 @@ handler = do
               H.a ! A.href (toValue ("/?q=" ++ T.unpack (fromJust query) ++ "&offset=" ++ show (offset + genericLength (getResults res)))) $ "there's more..."
         Nothing -> mempty
   where displayResult ev =
-          H.p ! A.class_ "message" $ do
+          let stripTrailing path
+                | head (reverse path) == '-' = init path
+                | otherwise                  = path
+              path = (formatTime defaultTimeLocale "%Y/%Y-%m/%Y-%m-%d.html#T%H-%M-%S" $ sayTimestamp ev) ++
+                     fPicoseconds (formatTime defaultTimeLocale "%Q" $ sayTimestamp ev)
+              fPicoseconds [] = ""
+              fPicoseconds p = "-" ++ tail p
+              href = "http://chatlogs.musicbrainz.org/" ++ (T.unpack $ sayChannel ev) ++ "/" ++ path
+              stamp = formatTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" $ sayTimestamp ev
+          in H.a ! A.class_ "message" ! A.href (toValue href) $ do
+            H.span ! A.class_ "time" $ toHtml   stamp
             H.span ! A.class_ "nick" $ toHtml $ sayUser ev
             H.span ! A.class_ "body" $ toHtml $ sayBody ev
         css = unlines [ "body { font-family: sans-serif; font-size: 12px }"
                       , ".nick { width: 8em; text-align: right; float: left; display: block; font-weight: bold; }"
-                      , ".body { margin-left: 11.5em; display: block }"
-                      , "p.message { clear: both; background: #f3f3f3; padding: 1em; border-radius: 5px }"
+                      , ".body { margin-left: 9em; margin-right: 13em; display: block }"
+                      , ".time { float:right; width: 12em; text-align: left }"
+                      , "a.message { clear: both; background: #f3f3f3; padding: 1em; border-radius: 5px; display: block; margin-bottom: 0.5em }"
+                      , "a { color: #000; text-decoration: none }"
+                      , "a:hover { background: #eaeaea }"
                       , "p.more { text-align: center; font-size: 18pt; border-top: 1px solid #eee; padding: 1em; color: #555; }"
                       ]
 
--- src/MBChatLogger/Parse.hs
+++ src/MBChatLogger/Parse.hs
@@ -6,7 +6,7 @@ import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text.Encoding as E
 
 import Data.Enumerator       (Iteratee)
-import Data.Text             (Text)
+import Data.Text             (Text, unpack)
 import Data.Time             (readTime)
 import Data.XML.Types        (Event)
 import System.Locale         (defaultTimeLocale, iso8601DateFormat)
@@ -19,16 +19,22 @@ parseEvent :: Text -> Iteratee Event IO (Maybe IRCEvent)
 parseEvent channel =
   nsTag rdf "li" $
     reqTagAttr foaf "chatEvent" (reqAttr rdf "ID") $ \rdfId -> do
-      date <- parseTimestamp `fmap` reqTag dc "date" content
+      date <- parseTimestamp rdfId `fmap` reqTag dc "date" content
       desc <- reqTag dc "description" content
       name <- reqTag dc "creator" $
                 reqTagAttr wn "Person" (reqAttr foaf "nick") $
                   \nick -> return nick
       many (nsTag dc "relation" $ return ())
       return $ Say name desc date channel
-  where parseTimestamp =
-          readTime defaultTimeLocale (iso8601DateFormat $ Just "%H:%M:%SZ") .
-            BS.unpack . E.encodeUtf8
+  where parseTimestamp timeString =
+          let picoseconds = drop 10 (unpack timeString)
+              reader = readTime defaultTimeLocale
+              doReadTime picoseconds
+                | null picoseconds = reader (formatString False) . init
+                | otherwise        = reader (formatString True) . (++ ("." ++ picoseconds)) . init
+          in doReadTime picoseconds . unpack
+        formatString False = iso8601DateFormat $ Just "%H:%M:%S"
+        formatString True = iso8601DateFormat $ Just "%H:%M:%S%Q"
 
 parseChatChannel :: Text -> Iteratee Event IO [IRCEvent]
 parseChatChannel channel =
